1bf6f7f972700e700b6adecb65b4fb4f5e201858
robhuzzey <github@differentelements.co.uk>
2023-02-19 20:46:11 +0000

    Added e2e tests on the component lib thingy

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,9 @@
+describe('spec.cy.js', () => {
+    it('should visit the homepage', () => {
+        cy.visit('/');
+    });
+    it("should have the test file included", () => {
+        cy.visit('/');
+        cy.get('.testComponent').contains('Number of results')
+    })
+});
--- src/Test.jsx
+++ src/Test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 export default ({ numberOfResults }) => (
-    <div style={{
+    <div className="testComponent" style={{
         border: "3px dashed blue"
     }}>Number of results: {numberOfResults}</div>
 );
